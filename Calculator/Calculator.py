ec6f51d7ae7715a0217bfed384c4a6ca99f9d2f3
anmorales-hub <moralesand2018@gmail.com>
2020-03-03 15:43:23 -0500

    Task: add division, exponent, log, square root functions and tests

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,3 +1,12 @@
+from MathOperations.addition import Addition
+from MathOperations.subtraction import Subtraction
+from MathOperations.multiplication import Multiplication
+from MathOperations.division import Division
+from MathOperations.exponent import Exponent
+from MathOperations.log import Log
+from MathOperations.root import Root
+
+
 class Calculator:
     Result = 0
     def __init__(self):
@@ -12,5 +21,21 @@ class Calculator:
 
     def Multiplication(self, a, b):
         self.Result = Multiplication.multiplication(a, b)
+        return self.Result
+
+    def Divide(self, a, b):
+        self.Result = Division.quotient(a, b)
+        return self.Result
+
+    def Root(self, a):
+        self.Result = Root.sqr_root(a)
+        return self.Result
+
+    def Power(self, a, b):
+        self.Result = Exponent.power(a, b)
+        return self.Result
+
+    def Logarithm(self, a, b):
+        self.Result = Log.logarithm(a, b)
         return self.Result
 
--- Tests/test_MathOperations.py
+++ Tests/test_MathOperations.py
@@ -3,6 +3,10 @@ import unittest
 from MathOperations.addition import Addition
 from MathOperations.subtraction import Subtraction
 from MathOperations.multiplication import Multiplication
+from MathOperations.division import Division
+from MathOperations.exponent import Exponent
+from MathOperations.log import Log
+from MathOperations.root import Root
 
 
 class MyTestCase(unittest.TestCase):
@@ -19,6 +23,18 @@ class MyTestCase(unittest.TestCase):
 
     def test_MathOperations_multiplication(self):
         self.assertEqual(2, Multiplication.multiplication(1, 2))
+
+    def test_MathOperations_division(self):
+        self.assertEqual(2, Division.quotient(10, 5))
+
+    def test_MathOperations_exponent(self):
+        self.assertEqual(16,Exponent.power(4,2))
+
+    def test_MathOperations_log(self):
+        self.assertEqual(0, Log.logarithm(10,1))
+
+    def test_MathOperations_root(self):
+        self.assertEqual(3, Root.sqr_root(9))
 
 
 if __name__ == '__main__':
