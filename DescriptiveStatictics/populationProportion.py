002c6f9796839d2d47dcea3e6a435bbfd19da7f0
Andrey Morales <55511896+anmorales-hub@users.noreply.github.com>
2020-03-12 21:09:21 -0400

    Fix: fix population proportion function

--- DescriptiveStatictics/populationProportion.py
+++ DescriptiveStatictics/populationProportion.py
@@ -4,6 +4,6 @@ from RNG.listPick import ListPick
 class PopulationProportion:
     @staticmethod
     def populationPorportion(seeds, nums, data):
-        p = listPick.listPickListSeed(seeds, nums, data)
+        p = ListPick.listPickListSeed(seeds, nums, data)
         pp = len(p) / len(data)
         return pp
