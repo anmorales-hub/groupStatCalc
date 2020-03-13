02cc2759b26a73aa4c4681dded691f70cd7d1a88
Andrey Morales <55511896+anmorales-hub@users.noreply.github.com>
2020-03-12 22:22:25 -0400

    Fix: update confidence interval sample function

--- PopulationSampling/confidenceIntervalSample.py
+++ PopulationSampling/confidenceIntervalSample.py
@@ -1,9 +1,9 @@
 from PopulationSampling.confidenceIntervalPopulation import ConfidenceIntervalPopulation
-from PopulationSampling.simpleRandSamp import SimpleRandSamp
+from PopulationSampling.simpRandSamp import SimpRandSamp
 
 
 class ConfidenceIntervalSample:
     @staticmethod
     def confidenceIntervalSample(confidence, seed, nums, data):
-        samp = SimpleRandSamp.listPickListSeed(seed, nums, data)
+        samp = SimpRandSamp.simpRandSamp(seed, nums, data)
         return ConfidenceIntervalPopulation.confidenceIntervalPopulation(confidence, samp)
