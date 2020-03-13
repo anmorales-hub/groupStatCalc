cc3c2a24bdcc77c678958030c9447df640f30d12
Andrey Morales <55511896+anmorales-hub@users.noreply.github.com>
2020-03-12 21:06:09 -0400

    Fix: fix sample correlation function errors

--- DescriptiveStatictics/samplecorrelation.py
+++ DescriptiveStatictics/samplecorrelation.py
@@ -6,7 +6,7 @@ from DescriptiveStatictics.covariance import Covariance
 class SampleCorrelation:
     @staticmethod
     def samplecorrelation(seed, sample_size, data1, data2):
-        dataA = listPick.listPickListSeed(seed, sample_size, data1)
-        dataB = listPick.listPickListSeed(seed, sample_size, data2)
+        dataA = ListPick.listPickListSeed(seed, sample_size, data1)
+        dataB = ListPick.listPickListSeed(seed, sample_size, data2)
 
         return Covariance.covariance(dataA, dataB) / (Stddev.stddev(dataA) * Stddev.stddev(dataB))
